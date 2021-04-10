2aff9e74bb9082f754cea43c81c53b551297cd84
Zxander <Zxander.Rodriguez@cognizant.com>
2021-04-10 10:14:32 -0500

    RED create playlist test

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# test_delete
+# Playlist_Service
+
+Rule: Playlist names are unique.
+Rule: Song is just the name of the song.
+
+When a playlist is created with a name
+Then a confirmation is returned that it was successful.
+And the playlist is empty.
+
+When a playlist is created with existing name
+Then a message is returned that it was unsuccessful.
+
+When a playlist is created without a name
+Then a message is returned that a name is required.
+
+Given a playlist
+When a song is added
+Then the playlist have one more song
+
+Given a playlist has songs
+When a song is removed
+Then the playlist have one less song
+
+Given a playlist has songs
+When retrieve the playlist
---
+++ src/test/java/com/galvanize/playlist/service/PlaylistService/IT/PlaylistServiceIT.java
@@ -0,0 +1,45 @@
+package com.galvanize.playlist.service.PlaylistService.IT;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+//@Transactional
+//@AutoConfigureRestDocs
+public class PlaylistServiceIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    /*    When a playlist is created with a name
+        Then a confirmation is returned that it was successful.
+        And the playlist is empty.*/
+    @Test
+    public void createPlaylist_SuccesMessage() throws Exception {
+
+        String playlistDTO = "{\"name\":\"myNewPlaylist\", \"playlist\":[]}";
+        mockMvc.perform(post("/guests")
+                .content(playlistDTO)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+//                .andDo(document("AddGuests"));
+
+    }
+
+}
