0cabb7b16a2cd269272e473af5773f437350cc5f
sebin <sebin.francis@cognizant.com>
2021-04-10 14:47:00 -0400

    deployed heroku app

---
+++ src/main/java/com/galvanize/playlist/service/PlaylistService/DatabaseConfig.java
@@ -0,0 +1,30 @@
+package com.galvanize.playlist.service.PlaylistService;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@Profile("heroku")
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder.create()
+                .url(dbUrl)
+                .username(username)
+                .password(password)
+                .build();
+    }
+}
