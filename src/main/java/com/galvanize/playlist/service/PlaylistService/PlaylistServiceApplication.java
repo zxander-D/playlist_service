59f68aac30bba8a6f6a82db3e770cc0aabb0aaf9
Zxander <Zxander.Rodriguez@cognizant.com>
2021-04-10 10:01:20 -0500

    init project

---
+++ src/main/java/com/galvanize/playlist/service/PlaylistService/PlaylistServiceApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.playlist.service.PlaylistService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/playlist/service/PlaylistService/PlaylistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.playlist.service.PlaylistService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
